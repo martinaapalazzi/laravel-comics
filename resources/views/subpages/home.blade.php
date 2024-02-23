24cb0745189375832ec33da8fef236fbf0dc1cdc
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-23 17:18:14 +0100

    edit

--- resources/views/subpages/home.blade.php
+++ resources/views/subpages/home.blade.php
@@ -6,9 +6,18 @@
 
 <section>
     <div class="container">
-        <div class="mycard">
-            
+        <div class="cards d-flex">
+            @foreach($comics as $index => $comic) 
+                <div class="mycard">
+                    <div class="img-container">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    </div>
+                    <div>
+                        {{ $comic['title']}}
+                    </div>
+                </div>
 
+            @endforeach
         </div>
     </div>
 </section>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('subpages.home');
+    $comics = config('comics');
+    return view('subpages.home', [
+        'comics' => $comics,
+    ]);
 });
 
 Route::get('/chi-siamo', function () {
