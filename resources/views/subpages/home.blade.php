45a03f3a65ec0e0b6a61a54699592c51e14271da
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-24 15:25:56 +0100

    add after jumbotron

--- resources/views/subpages/home.blade.php
+++ resources/views/subpages/home.blade.php
@@ -23,5 +23,51 @@
         </div>
     </div>
 </section>
+<section>
+    <div class="container-assets d-flex justify-content-around">
+        <div class="d-flex justify-content-around align-items-center">
+            <div class="single-asset d-flex align-items-center">
+                <div class="img-asset-container">
+                    <img src="{{ Vite::asset('public/images/buy-comics-digital-comics.png') }}" alt="jumbotron">
+                </div> 
+                <div>
+                    DIGITAL COMICS
+                </div>
+            </div>
+            <div class="single-asset d-flex">
+                <div class="img-asset-container">
+                    <img src="{{ Vite::asset('public/images/buy-comics-merchandise.png') }}" alt="jumbotron">
+                </div> 
+                <div>
+                    DC MERCHANDISE
+                </div>
+            </div>
+            <div class="single-asset d-flex">
+                <div class="img-asset-container">
+                    <img src="{{ Vite::asset('public/images/buy-comics-subscriptions.png') }}" alt="jumbotron">
+                </div> 
+                <div>
+                    SUBSCRIPTION
+                </div>
+            </div>
+            <div class="single-asset d-flex">
+                <div class="img-asset-container">
+                    <img src="{{ Vite::asset('public/images/buy-comics-shop-locator.png') }}" alt="jumbotron">
+                </div> 
+                <div>
+                    COMIC SHOP LOCATOR
+                </div>
+            </div>
+            <div class="single-asset d-flex">
+                <div class="img-asset-container">
+                    <img src="{{ Vite::asset('public/images/buy-dc-power-visa.svg') }}" alt="jumbotron">
+                </div> 
+                <div>
+                    DC POWER VISA
+                </div>
+            </div>
+        </div>
+    </div>      
+ </section>
 
 @endsection
